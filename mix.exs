fc4576a7c7e5473f25d2058ce6cec2502c757373
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-08-30 02:49:10 +0100

    feat: install mix audit

--- mix.exs
+++ mix.exs
@@ -77,7 +77,8 @@ defmodule Helix.MixProject do
       {:excoveralls, "~> 0.18", only: :test},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
-      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false}
     ]
   end
 
