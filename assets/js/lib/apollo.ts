6be472904fbcc791bd35e48574adae7d1e758f8a
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-09-15 08:39:38 +0100

    feat: add GraphQL client setup and development tools
    - Install Apollo Client and GraphQL core dependencies
    - Add GraphQL Code Generator for TypeScript generation
    - Create Apollo Client configuration with authentication
    - Add codegen scripts for schema introspection
    - Ignore .DS_Store files in .gitignore

---
+++ assets/js/lib/apollo.ts
@@ -0,0 +1,31 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/api/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      errorPolicy: 'ignore',
+    },
+    query: {
+      errorPolicy: 'ignore',
+    },
+  },
+});
